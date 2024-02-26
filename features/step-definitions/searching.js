204a1636810026fa1f990e74497cc490c3806527
Irina <irinafedjko@gmail.com>
2024-02-26 20:31:21 +0200

    search.js added

--- features/step-definitions/searching.js
+++ features/step-definitions/searching.js
@@ -4,42 +4,39 @@ import page from "../page-objects/page.js";
 import searchPage from "../page-objects/search.page.js";
 
 When('I enter {string} into search field', async function(word) {
-    // await expect(searchPage.wordInput)
-        // .toHaveText(expect.stringContaining(this.word));
-        await searchPage.wordInput.setValue(word);
+    await searchPage.wordInput.setValue(word);
 });
 
 When('I click search button', async function() {
     await searchPage.searchButton.click();
 });
 
-// Then('Page appears only with products containing text {string}', async function(word){
-//     const inputWord = await searchPage.inputWord;
-//     this.inputWordSearch = await inputWord.getText();
-//     await expect(searchPage.inputWord)
-//     .toHaveText(expect.stringContaining(this.inputWordSearch));
-//         const element = searchPage.inputWord($('#center_column .product-name'));
-//         const elementText = element.textContent.trim();
-
-//     Then('Page appears only with products containing text {string}', async function(word){
-
-//         if (elementText.includes(word)) {
-//             await searchPage.inputWord.toBeDisplayed();
-//         } else {
-//             throw Error(` ${word} not in the stock`);
-//         }
-        
 
 
-// Get the element using the selector
-// Then('Page appears only with products containing text {string}', async function(word) {
-//     if (!Object.keys(inputWord).includes(word)) {
-//         throw Error(` ${word} is not in the stock`);
-//     }
+// Then('Page appears only with products containing text "Blouse"', async function() {
+// const inputWord = await searchPage.inputWord;
+// const elementText = await inputWord.getText();
+// const myString = 'Blouse';
 
-//     searchPage.inputWord() = ('#center_column .product-name');
-//     });
+// if(elementText === myString) {
+//   console.log('The texts match!');
+// //   await inputWord.toBeDisplayed();
+// } else {
+//     console.log('The texts do not match. Element text is:', elementText);
+// }
 
-// const element = await $('#center_column .product-name'); // Replace 'your-selector' with the actual CSS selector
-// const idAttributeValue = await element.getAttribute('id');
-// console.log('ID attribute value:', idAttributeValue);
+// });
+
+
+
+
+Then('Page appears only with products containing text {string}', async function(word) {
+const inputWord = await searchPage.inputWord;
+const elementText = await inputWord.getText();
+
+if(elementText === word) {
+  console.log('The texts match!');
+} else {
+    console.log('The texts do not match. Element text is:', elementText);
+}
+});
