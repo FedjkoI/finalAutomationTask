00ec53825b03969086b342211e64df2a28340529
Irina <irinafedjko@gmail.com>
2024-02-28 17:37:16 +0200

    adding.js correction

--- features/step-definitions/adding.js
+++ features/step-definitions/adding.js
@@ -7,7 +7,7 @@ import addingPage from "../page-objects/adding.page.js";
 When('I see one Product in Cart', async function() {
     let quantity = '1';
     let cartQuantity = await addingPage.cartQuantity;
-    let quantityOfPurchase = await cartQuantity.getAttribute('1');
+    let quantityOfPurchase = await cartQuantity.getAttribute('input.cart_quantity_input');
 if (quantityOfPurchase == quantity){
     console.log('The nomber match!');
 }else{
@@ -24,11 +24,13 @@ When('I press Plus button to add second product', async function() {
 Then('Cart will increase on 1 Product as well', async function() {
     let quantity = '2';
     let cartQuantity = await addingPage.cartQuantity;
-    let quantityOfPurchase = await cartQuantity.getAttribute('2');
+    let quantityOfPurchase = await cartQuantity.getAttribute('input.cart_quantity_input');
 if (quantityOfPurchase == quantity){
     console.log('The nomber match!');
 }else{
     console.log('The number do not match. Element text is:', quantityOfPurchase);
 }
 });
+
+
 
